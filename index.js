5aae7e5b124c2e8eb85e6a0daab61747223874c5
Matt Lilley <m.k.lilley@gmail.com>
2022-10-17 09:27:37 +0100

    Fixed bugs

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const outOfStockText = "Back in stock soon";
 const url =
   "https://ao.com/product/eib15050a1duk1-indesit-fridge-freezer-white-79565-28.aspx";
 
-const job = schedule.scheduleJob("*/1 * * * *", function (fireDate) {
+const job = schedule.scheduleJob("*/5 * * * *", function (fireDate) {
   console.log(fireDate);
   checkIfOutOfStock();
 });
+
 
 async function checkIfOutOfStock() {
   try {
@@ -19,23 +20,25 @@ async function checkIfOutOfStock() {
     if (id == -1) {
       //send alert
       console.log("In stock");
+      await telegramAlert("In stock");
     } else {
       console.log("Out of stock");
-      await telegramAlert();
+      await telegramAlert("Out of stock");
     }
   } catch (e) {
-    // send alert that something has gone wrong with axios
+    console.log(e)
+    await telegramAlert("**Error:** failed to check if item is in stock");
   }
 }
 
-async function telegramAlert() {
+async function telegramAlert(text) {
   const payload = {
-    chat_id: "@urgent_lilley",
+    chat_id: process.env.CHAT_ID,
     parse_mode: "Markdown",
-    text: "**TEST**",
+    text: text,
   };
   try {
-    const response = axios.post(
+    const response = await axios.post(
       `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`,
       payload
     );
